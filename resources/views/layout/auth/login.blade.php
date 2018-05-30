e966c5c771cddbd2ffd154d552e7c94defeb18ef
kusamops <katy.bas@mail.ru>
2018-05-31 00:27:01 +0300

    make login view

---
+++ resources/views/layout/auth/login.blade.php
@@ -0,0 +1,35 @@
+@extends('layout.master')
+
+@section('title')
+	Admin Login
+@endsection
+
+@section('content')
+<div class="container">
+	
+	<form action="{{ route('login') }}" method="POST">
+		{{ csrf_field() }}
+
+		<h2>Signing in</h2>
+		
+		<div class="form-group">
+			@include('layout.errors')
+		</div>
+
+		<div class="form-group">
+			<label for="email">Email Adress:</label>
+			<input type="email" class="form-control col-sm-5" id="email" name="email" required>	
+		</div>
+
+		<div class="form-group">
+			<label for="password">Password:</label>
+			<input type="password" class="form-control col-sm-5" id="password" name="password" required>	
+		</div>
+
+		<div class="form-group">
+			<button type="submit" class="btn btn-default">Sign in</button>
+		</div>
+	</form>
+
+</div>
+@endsection
