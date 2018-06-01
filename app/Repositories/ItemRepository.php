b4493a3dffaf0aa9aa56454ea928f8d7a61a869c
casper <ycasperinho@gmail.com>
2018-06-01 10:52:30 +0300

    change the way of saving item

--- app/Repositories/ItemRepository.php
+++ app/Repositories/ItemRepository.php
@@ -8,13 +8,9 @@ use App\Item;
 class ItemRepository extends Repository{
 	public function create($array){
 
-	$item = new Item;
+	$item = new Item($array);
 
 	$item->category_id = $array['category_id'];
-	$item->title = $array['title'];
-	$item->description = $array['description'];
-	$item->price = $array['price'];
-    $item->photo = $array['photo'];
 
 	$item->save();
 
